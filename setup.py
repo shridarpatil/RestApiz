89ad1c2a1d43790eeb6466bd80444abdc4625bee
shri <sp@routo.co>
2017-03-09 15:15:34 +0530

    Change of package name

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'restapi',
-  packages = ['restapi'], # this must be the same as the name above
-  version = '0.0.1',
+  name = 'RestApiz',
+  packages = ['RestApiz'], # this must be the same as the name above
+  version = '0.0.2',
   description = 'Create Rest Api',
   author = 'Shridhar Patil',
   author_email = 'shridharpatil2792@gmail.com',
   url = 'https://github.com/shridarpatil/Flask-RestApi', #URL to the github repo
-  download_url = 'https://github.com/shridarpatil/Flask-RestApi/archive/0.0.1.tar.gz',
+  download_url = 'https://github.com/shridarpatil/Flask-RestApi/archive/0.0.2.tar.gz',
   keywords = ['RestApi', 'rest api', 'Rest Api'], # arbitrary keywords
   classifiers = [],
   install_requires=[
