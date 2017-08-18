278a0b4cb47d098776c6d2be7aeffb54fd379a39
shridhar <shridhar.p@zerodha.com>
2017-08-18 12:59:37 +0530

    Docs add import statements

--- docs/conf.py
+++ docs/conf.py
@@ -30,9 +30,8 @@ project_root = os.path.dirname(cwd)
 # This lets us ensure that the source package is imported, and that its
 # version is used.
 sys.path.insert(0, project_root)
-autodoc_mock_imports = ['Flask']
 
-import RestApiz
+# import RestApiz
 
 # -- General configuration ---------------------------------------------
 
